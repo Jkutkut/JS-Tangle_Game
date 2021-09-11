bd5d1f5fb89ab98a5b9cab4a499e87fdfea4cba3
= <=>
2021-09-11 16:58:42 +0200

    Now the points appear within a margin using an offset

--- Classes/net.js
+++ Classes/net.js
@@ -1,14 +1,32 @@
 class Net {
-    constructor(size=10) {
+    offset = 0.1;
+    grid = 20;
+
+    constructor(size, width, height) {
         this._size = size;
+        this.screenSize = [width, height]; //.map(x => x * (1 - this.offset * 2));
         
-        var points = new Set();
+        this._points = new Set();
 
         this.createNewNet();
     }
 
     createNewNet() {
         this.points.clear()
+
+        for (let i = 0; i < this.size; i++) {
+            let coord = [0, 0];
+            for (let j = 0; j < 2; j++) {
+                coord[j] = (Math.random() * (1 - this.offset * 2) + this.offset) * this.screenSize[j];
+            }
+
+            this._points.add(new Point(...coord));
+        }
+    }
+
+    // GETTERS
+    get size() {
+        return this._size;
     }
 
     get points() {
